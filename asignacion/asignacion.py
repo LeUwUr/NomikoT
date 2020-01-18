ee69a09c15d93b092489045a40f4876bd2166d9b
LeUwUr <evadevizcarra@gmail.com>
2020-01-17 22:55:50 -0800

    owo

---
+++ asignacion/asignacion.py
@@ -0,0 +1,34 @@
+numero = 10
+print(numero)
+print(type(numero))
+
+cadena = 'usfjgbg'
+print (cadena)
+print(type(cadena))
+
+owo = "estoy 'estudiando'"
+print (owo)
+print(type(owo))
+
+num1 = 10
+num2 = 6.7
+suma = num1+num2
+print(suma)
+
+num3 = 10
+num4 = 6.7
+suma2 = num3+num4 * 10 / 6
+print(suma2)
+
+num5 = 10
+num6 = 6.7
+suma3 = (num5+num6) * 10 / 6
+print("la suma es:", suma3)
+
+valor = 14
+print(valor)
+
+valor = "Eva"
+print(valor)
+
+
