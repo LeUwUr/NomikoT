908fc0aa5b85f8748d0237e3b8cad90ad8f46daa
LeUwUr <evadevizcarra@gmail.com>
2020-01-17 23:01:00 -0800

    khe

---
+++ operadorR/relacionales.py
@@ -0,0 +1,26 @@
+# Operadores Relacionales
+
+resultado = 10 < 20
+print(resultado)
+
+resultado1 = 10 > 20
+print(resultado1)
+
+resultado3 = 10 == 20
+print(resultado3)
+
+resultado4 = 10 != 20
+print(resultado4)
+
+resultado5 = 10 <= 20
+print(resultado5)
+
+resultado6 = 10 >= 20
+print(resultado6)
+
+a = 10
+b = 20
+c = 30 
+resultadoA = a+b == c
+print(resultadoA)
+
