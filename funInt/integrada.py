947856d7185d82b8f7c9da0ec969eb00084b1e96
LeUwUr <evadevizcarra@gmail.com>
2020-01-17 22:58:05 -0800

    7u7

---
+++ funInt/integrada.py
@@ -0,0 +1,24 @@
+# Funciones integradas
+
+n = str(10)
+	print(n)
+
+{m = bin(10)
+	print(m)}
+
+{h = hex(10)
+	print(h)}
+
+{d = int("0b1010",2)
+	print(d)}
+
+{a = abs(-8)
+	print(a)}
+
+{b = round(6.6)
+	print(b)}
+
+{g = len("Mariana") # Saber cuantas letras hay en una palabra
+	print(g)}
+
+
