d6366e18f96203bcd43bc4730c924bd419b34989
LeUwUr <evadevizcarra@gmail.com>
2020-01-17 22:53:22 -0800

    :3

---
+++ OperadorL/logico.py
@@ -0,0 +1,18 @@
+# Operadores logicos
+
+a = 10
+b = 15
+c = 20
+
+resultado = ((a<b) and (b>c))
+print(resultado)
+
+resultado1 = ((a>b) and (b<c))
+print(resultado1)
+
+resultado2 = ((a>b) or (b<c))
+print(resultado2)
+
+resultado3 = not ((a>b) or (b<c))
+print(resultado3)
+
